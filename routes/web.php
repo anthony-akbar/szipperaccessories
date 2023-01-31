518eb632c362127e3450e018cb37581bb86b7148
ramziddin abdumominov <ramziddinabdumominov707@gmail.com>
2023-01-31 14:29:02 +0500

    fronted navbar va home va slider qilindi

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
 use App\Http\Controllers\admin\DashboardController;
+use App\Http\Controllers\front\SliderController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['prefix'=>'admin', 'namespace'=>'admin'], function () {
-    Route::get('/', [DashboardController::class,'index'])->name('admin');
+Route::group(['prefix' => 'admin', 'namespace' => 'admin'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('admin');
 });
 
 Route::get('/', function () {
     return view('front.home.index');
+});
+Route::group(['prefix' => 'slider'], function () {
+    Route::get('/', [SliderController::class, 'index'])->name('front.slider');
 });
