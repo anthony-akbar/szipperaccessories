659d3cd23e1d1e7936b6d2d3f67a0a2ccad5f17d
ramziddin abdumominov <ramziddinabdumominov707@gmail.com>
2023-01-31 15:25:45 +0500

    Front categories qilinvoti

--- resources/views/front/slider/index.blade.php
+++ resources/views/front/slider/index.blade.php
@@ -2,19 +2,17 @@
 <section id="hero" class="hero">
 
 
-
     <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
-{{--        @foreach($slides as $slider)--}}
+        @foreach($sliders as $slider)
             <div class="carousel-item active"
-                 style="background-image: url(assets/img/hero-carousel/hero-carousel-1.jpg)">
+                 style="background-image: url({{'storage/'.$slider->image}})">
                 <div class="info d-flex align-items-center">
                     <div class="container">
                         <div class="row justify-content-center">
                             <div class="col-lg-6 text-center">
-                                <h2 data-aos="fade-down">Welcome to <span>UpConstruction</span></h2>
-                                <p data-aos="fade-up">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                    ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                {{--                                <span>UpConstruction</span>--}}
+                                <h2 data-aos="fade-down">{{$slider->title}}</h2>
+                                <p data-aos="fade-up">{{$slider->description}}</p>
                                 <a data-aos="fade-up" data-aos-delay="200" href="#get-started" class="btn-get-started">Get
                                     Started</a>
                             </div>
@@ -22,15 +20,6 @@
                     </div>
                 </div>
             </div>
-            <div class="carousel-item"
-                 style="background-image: url(assets/img/hero-carousel/hero-carousel-2.jpg)"></div>
-            <div class="carousel-item"
-                 style="background-image: url(assets/img/hero-carousel/hero-carousel-3.jpg)"></div>
-            <div class="carousel-item"
-                 style="background-image: url(assets/img/hero-carousel/hero-carousel-4.jpg)"></div>
-            <div class="carousel-item"
-                 style="background-image: url(assets/img/hero-carousel/hero-carousel-5.jpg)"></div>
-
             <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
             </a>
@@ -38,7 +27,7 @@
             <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
                 <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
             </a>
-{{--        @endforeach--}}
+        @endforeach
     </div>
 
 </section><!-- End Hero Section -->
--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,20 @@
 
 use App\Http\Controllers\admin\DashboardController;
 use App\Http\Controllers\admin\SliderController;
-use App\Http\Controllers\front\SliderController;
+use App\Models\SliderItem;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['prefix'=>'admin', 'namespace'=>'admin'], function () {
-    Route::get('/', [DashboardController::class,'index'])->name('admin');
+Route::group(['prefix' => 'admin', 'namespace' => 'admin'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('admin');
     Route::group(['prefix' => 'slider'], function () {
         Route::get('/', [SliderController::class, 'index'])->name('admin.slider');
         Route::post('/store', [SliderController::class, 'store'])->name('admin.slider.store');
         Route::delete('/{id}', [SliderController::class, 'destroy'])->name('admin.slider.delete');
     });
-Route::group(['prefix' => 'admin', 'namespace' => 'admin'], function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('admin');
 });
 
 Route::get('/', function () {
-    return view('front.home.index');
-});
-Route::group(['prefix' => 'slider'], function () {
-    Route::get('/', [SliderController::class, 'index'])->name('front.slider');
+    $sliders = SliderItem::all();
+    return view('front.home.index', compact('sliders'));
 });
