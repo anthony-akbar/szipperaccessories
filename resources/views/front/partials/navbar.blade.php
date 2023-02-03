e184ed26a0a33b75a1612fa43952d3c583747d8e
Anthony Akbar <mamadjanovakbar@gmail.com>
2023-02-03 21:21:41 +0500

    Logo

--- resources/views/front/partials/navbar.blade.php
+++ resources/views/front/partials/navbar.blade.php
@@ -2,10 +2,8 @@
 <header id="header" class="header d-flex align-items-center">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-        <a href="index.html" class="logo d-flex align-items-center">
-            <!-- Uncomment the line below if you also wish to use an image logo -->
-            <!-- <img src="assets/img/logo.png" alt=""> -->
-            <h1>UpConstruction<span>.</span></h1>
+        <a href="/" class="logo d-flex align-items-center">
+            <img src="logo.svg">
         </a>
 
         <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
